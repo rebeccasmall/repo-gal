640662cdc8a8342aa8de668c422e9f4237d0ce4b
rebeccasmall <rebecca.small18@hotmail.com>
2025-07-03 13:10:53 -0400

    Fetched user data fromgithub API

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 // Div where profile information will appear 
 const overview = document.querySelector(".overview");
+
+const username = "rebeccasmall";
+
+const getData = async function () {
+    const res = await fetch (`https://api.github.com/users/${username}`);
+    const data = await res.json();
+    console.log(data);
+}
+
+getData();
