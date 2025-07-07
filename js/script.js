405b933ea6dcc4f7e6bd1c85fe5093cb5fad2eef
rebeccasmall <rebecca.small18@hotmail.com>
2025-07-07 17:42:58 -0400

    Called function to retrieve data inside the async function

--- js/script.js
+++ js/script.js
@@ -7,6 +7,23 @@ const getData = async function () {
     const res = await fetch (`https://api.github.com/users/${username}`);
     const data = await res.json();
     console.log(data);
+    displayUserInfo(data);
 }
 
 getData();
+
+const displayUserInfo = function (data) {
+    let userDiv = document.createElement("div");
+    userDiv.classList.add("user-info");
+    userDiv.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div> `
+    overview.append(userDiv);
+};
